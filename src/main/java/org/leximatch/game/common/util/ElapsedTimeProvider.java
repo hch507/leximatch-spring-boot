c02bb74534ba85e77fcba198c649b66ec2d3d32f
hch507 <chunghuihan9@gmail.com>
2026-05-13 13:17:08 +0900

    fix : ElapsedTimeProvider 한국시간으로 수정

--- src/main/java/org/leximatch/game/common/util/ElapsedTimeProvider.java
+++ src/main/java/org/leximatch/game/common/util/ElapsedTimeProvider.java
@@ -4,15 +4,23 @@ import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Component
 public class ElapsedTimeProvider {
 
+    private static final ZoneId KOREA_ZONE = ZoneId.of("Asia/Seoul");
+
     public String getElapsedTimeFromMidnight() {
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime midnight = now.toLocalDate().atStartOfDay();
+
+        ZonedDateTime now = ZonedDateTime.now(KOREA_ZONE);
+
+        ZonedDateTime midnight = now.toLocalDate()
+                .atStartOfDay(KOREA_ZONE);
 
         Duration duration = Duration.between(midnight, now);
+
         long seconds = duration.getSeconds();
 
         long hh = seconds / 3600;
