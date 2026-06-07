2719f1e34d6d2d55927ca7f43c0dd1cb73af6f3e
hch507 <chunghuihan9@gmail.com>
2026-06-08 00:13:03 +0900

    refactor : hintier 정책 변경

--- src/main/java/org/leximatch/game/domain/policy/hint/HintTier.java
+++ src/main/java/org/leximatch/game/domain/policy/hint/HintTier.java
@@ -8,10 +8,10 @@ import lombok.RequiredArgsConstructor;
 public enum HintTier {
 
     TOP_10(1, 10, 3),
-    TOP_50(11, 50, 5),
-    TOP_100(51, 100, 15),
+    TOP_50(11, 50, 17),
+    TOP_100(51, 100, 30),
     TOP_300(101, 300, 35),
-    TOP_500(301, 500, 40);
+    TOP_500(301, 500, 15);
 
     private final int minRank;
     private final int maxRank;
