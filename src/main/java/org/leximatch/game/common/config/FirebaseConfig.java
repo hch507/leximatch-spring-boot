9dd4b4cd4aee35231723b810faaed2c6c491dc1f
hch507 <chunghuihan9@gmail.com>
2026-06-21 22:07:53 +0900

    feat : firebaseConfig 추가

---
+++ src/main/java/org/leximatch/game/common/config/FirebaseConfig.java
@@ -0,0 +1,38 @@
+package org.leximatch.game.common.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void init() throws IOException {
+
+        if (!FirebaseApp.getApps().isEmpty()) {
+            return;
+        }
+
+        GoogleCredentials credentials =
+                GoogleCredentials.fromStream(
+                        new ClassPathResource(
+                                "leximatch-77825-firebase-adminsdk-fbsvc-c0f625d686.json"
+                        ).getInputStream()
+                );
+
+        FirebaseOptions options =
+                FirebaseOptions.builder()
+                        .setCredentials(credentials)
+                        .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+}
