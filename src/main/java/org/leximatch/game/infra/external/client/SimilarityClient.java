71ced625f9da333bd705d8416d77d694daf88054
hch507 <chunghuihan9@gmail.com>
2026-08-26 23:40:15 +0900

    fix : 불필요 의존성 제거

--- src/main/java/org/leximatch/game/infra/external/client/SimilarityClient.java
+++ src/main/java/org/leximatch/game/infra/external/client/SimilarityClient.java
@@ -5,11 +5,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.leximatch.game.common.api.Api;
 import org.leximatch.game.infra.external.FastApiClientSupport;
-import org.leximatch.game.infra.external.dto.SimilarityRequest;
 import org.leximatch.game.infra.external.dto.SimilarityResponse;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
 @Slf4j
 @Component
 @RequiredArgsConstructor
